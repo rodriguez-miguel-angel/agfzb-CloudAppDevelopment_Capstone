7b9ef32fab4955aacc57fb8c2d13a9b91d0779a3
rodriguez-miguel-angel <rodriguez.miguel.angel@gmail.com>
2022-07-07 11:46:50 -0700

    module-02: modified files

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -59,7 +59,7 @@ def logout_request(request):
 def registration_request(request):
     context = {}
     if request.method == 'GET':
-        return render(request, 'djangoapp/user_registration_bootstrap.html', context)
+        return render(request, 'djangoapp/registration.html', context)
     elif request.method == 'POST':
         # Check if user exists
         username = request.POST['username']
@@ -79,7 +79,7 @@ def registration_request(request):
             return redirect("djangoapp:index")
         else:
             context['message'] = "User already exists."
-            return render(request, 'djangoapp/user_registration_bootstrap.html', context)
+            return render(request, 'djangoapp/registration.html', context)
 
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
